376b691b841047b8722368d1352f71d7d72638b9
Lucas <lucasverissimo1799@gmail.com>
2026-07-25 13:49:54 -0300

    Adiciona chaves para ambientes

---
+++ env/Dev/main.tf
@@ -0,0 +1,10 @@
+module "aws_dev" {
+  source = "../../infra"
+  instancia = "t3.micro"
+  regiao_aws = "us-west-2"
+  chave = "project-dev"
+}
+
+output "IP" {
+  value = module.aws_dev.IP_publico
+}
